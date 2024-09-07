ac46fc2692e81892011941769b216f6daf6bd75b
leeee36 <si859517@gmail.com>
2024-09-07 10:08:57 +0900

    add #

--- version2.py
+++ version2.py
@@ -1,18 +1,18 @@
 import shutil
 import os 
 
-dir = '/Users/sangwooklee/Desktop/photography/photo/DIGITAL/RICOH/9월/beagle_party/'
+dir = '/Users/sangwooklee/Desktop/photography/photo/DIGITAL/RICOH/9월/test/' # 폴더 경로
 files = os.listdir(dir)
 
 # ===== Make Dir ===== #
 
-jpg = dir + "/JPG"
-dng = dir + "/DNG"
+jpg = dir + "JPG"   # 파일명1 설정
+dng = dir + "DNG"   # 파일명2 설정
 
 try : 
     os.mkdir(jpg)
     os.mkdir(dng)
-except FileExistsError:
+except FileExistsError :
     pass
 
 # ===== Move Files ===== #
@@ -21,9 +21,9 @@ jpg_files = [file for file in files if file.find('.JPG') != -1]
 dng_files = [file for file in files if file.find('.DNG') != -1]
 
 for file in jpg_files:
-    new_path = dir + 'JPG/' + file
+    new_path = jpg + '/' + file
     shutil.move(dir+file, new_path)
 
 for file in dng_files:
-    new_path = dir + 'DNG/' + file
-    shutil.move(dir+file, new_path)    
+    new_path = dng + '/' + file
+    shutil.move(dir+file, new_path)
